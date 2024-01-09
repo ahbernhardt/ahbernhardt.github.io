cce93b70cb76e2001ec2e909a36e6bd5d95c074f
ahbernhardt <anhbernhardt@gmail.com>
2024-01-08 19:34:33 -0600

    updated: packages and formatted files with prettier

--- pages/_document.js
+++ pages/_document.js
@@ -1,21 +1,24 @@
-import React from 'react'
-import {Html, Head, NextScript, Main} from 'next/document'
+import React from "react";
+import { Html, Head, NextScript, Main } from "next/document";
 import Background from "/components/background/background";
 
 export default function Document() {
   return (
     <Html lang="en" className="bg-[#0c0c0c]">
-      <Head >
-          {/* eslint-disable-next-line @next/next/google-font-display */}
+      <Head>
+        {/* eslint-disable-next-line @next/next/google-font-display */}
         {/*<link href="https://fonts.googleapis.com/css2?family=Josefin+Slab" rel="stylesheet" />*/}
         {/*<link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Josefin+Slab&display=swap" rel="stylesheet"/>*/}
-        <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Josefin+Slab:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Poppins:ital,wght@0,100;0,400;0,700;0,900;1,100;1,400;1,700;1,900&display=swap" rel="stylesheet"/>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Josefin+Slab:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Poppins:ital,wght@0,100;0,400;0,700;0,900;1,100;1,400;1,700;1,900&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
-        <Background/>
+        <Background />
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,68 +1,65 @@
-import React from 'react'
-import SEO from "components/seo/seo"
+import React from "react";
+import SEO from "components/seo/seo";
 import Nav from "/components/nav/nav";
 import Carousel from "/components/carousel/Carousel";
 
-
 const images = [
-    "https://duruthemes.com/demo/html/fd/images/slider/01.jpg",
-    "https://duruthemes.com/demo/html/fd/images/slider/02.jpg",
-    "https://duruthemes.com/demo/html/fd/images/slider/03.jpg",
+  "https://duruthemes.com/demo/html/fd/images/slider/01.jpg",
+  "https://duruthemes.com/demo/html/fd/images/slider/02.jpg",
+  "https://duruthemes.com/demo/html/fd/images/slider/03.jpg",
 ];
 
 export default function Home() {
-    const site = {
-        title: "Home",
-        author: "Anh Bernhardt",
-        description: "Welcome to my portfolio",
-        url: "https://ahbernhardt.github.io/",
-        social: {
-            cardType: "summary_large_image",
-            url: "https://ahbernhardt.github.io",
-        },
-        image: "",
-    };
+  const site = {
+    title: "Home",
+    author: "Anh Bernhardt",
+    description: "Welcome to my portfolio",
+    url: "https://ahbernhardt.github.io/",
+    social: {
+      cardType: "summary_large_image",
+      url: "https://ahbernhardt.github.io",
+    },
+    image: "",
+  };
 
+  return (
+    <>
+      <SEO site={site} />
+      <Nav />
 
+      <main className="mx-auto my-12 flex max-w-[1422px] flex-col justify-center sm:my-6 xl:my-6 3xl:my-12">
+        <div className="z-1 text-center font-abril text-[4rem] font-extrabold uppercase leading-none text-white sm:text-[4.8rem] xl:text-[5.6rem] 3xl:text-[8.4rem]">
+          Welcome
+        </div>
 
-    return (
-        <>
-            <SEO site={site}/>
-            <Nav/>
+        <div className="z-1 mb-6 px-3 text-center sm:px-0">
+          <h3>
+            Thank you for checking out my portfolio. This version is in-process.
+          </h3>
+          <h3>
+            In the mean time, please visit{" "}
+            <a
+              href="https://anhbernhardt.com"
+              target="_blank"
+              rel="noreferrer"
+              className="cursor-pointer text-[#FF6A67] hover:text-orange-400"
+            >
+              anhbernhardt.com
+            </a>
+          </h3>
+        </div>
 
-            <main className="flex flex-col my-12 sm:my-6 xl:my-6 3xl:my-12 max-w-[1422px] justify-center mx-auto">
-                <div className="font-abril text-[4rem] sm:text-[4.8rem] xl:text-[5.6rem] 3xl:text-[8.4rem] text-white font-extrabold uppercase text-center leading-none z-1">
-                    Welcome
-                </div>
+        <div className="mx-auto mt-6 w-11/12 sm:mt-20 ">
+          <Carousel images={images} />
+        </div>
+      </main>
 
-                <div className="px-3 sm:px-0 text-center mb-6 z-1">
-                    <h3>Thank you for checking out my portfolio. This version is in-process.</h3>
-                    <h3>In the mean time, please visit {' '}
-                        <a href="https://anhbernhardt.com"
-                           target="_blank"
-                           rel="noreferrer"
-                           className="text-[#FF6A67] hover:text-orange-400 cursor-pointer">
-                            anhbernhardt.com</a>
-                    </h3>
-                </div>
-
-
-                <div className="w-11/12 mx-auto mt-6 sm:mt-20 ">
-                    <Carousel images={images}/>
-                </div>
-            </main>
-
-
-            <footer className="absolute bottom-0 w-full">
-                <div className="w-full z-10 font-neueHaas font-thin mx-auto max-w-[1422px] py-2 px-3 sm:px-6 flex text-[10px] sm:text-xs tracking-[0.5px] border-t-[1px] border-gray-200 uppercase justify-between">
-                    <div className="w-fit">
-                        The personal site & portfolio
-                    </div>
-                    <div className="w-fit">
-                        Anh Bernhardt
-                    </div>
-                </div>
-            </footer>
-        </>
-    )
+      <footer className="absolute bottom-0 w-full">
+        <div className="z-10 mx-auto flex w-full max-w-[1422px] justify-between border-t-[1px] border-gray-200 px-3 py-2 font-neueHaas text-[10px] font-thin uppercase tracking-[0.5px] sm:px-6 sm:text-xs">
+          <div className="w-fit">The personal site & portfolio</div>
+          <div className="w-fit">Anh Bernhardt</div>
+        </div>
+      </footer>
+    </>
+  );
 }
