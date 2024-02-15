c1d5e287575fbfc01eede5acbff3f6b0bfa95a80
ahbernhardt <anhbernhardt@gmail.com>
2024-02-14 22:59:45 -0600

    Updates

---
+++ static/IFIx0t6iCrSX7FrkpftYX/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/795-7b38b5f8074134a2.js","static/chunks/pages/index-7f9a72f08f3a19d7.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
